0bbe72543a3a76a7ca82ce6d955fcc80d5240d3b
victor diaz <vdmpeniel@yahoo.es>
2023-06-22 11:36:09 -0400

    Corrected error in TernaryOperator

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
+    public int max(int a, int b){
+        return a > b ? a : b;
+    }
+
     public void printGreatest(int first, int second, int third) {
+        System.out.println(max(max(first, second), third));
     }
 }
